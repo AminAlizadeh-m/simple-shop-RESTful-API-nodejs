bfdc6904ee9f2da56661424eb58019c9f6048f31
aminalizadeh.developer <aminalizadeh.developer@gmail.com>
2020-12-16 20:15:00 +0330

    Error handling added

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
-const productsRoutes = require('./api/products/products');
-const ordersRouter = require('./api/products/orders');
+const productsRoutes = require('./api/routes/products');
+const ordersRoutes = require('./api/routes/orders');
+
+app.use(morgan('dev'));
 
 app.use('/products', productsRoutes);
-app.use('/orders', ordersRouter);
+app.use('/orders', ordersRoutes);
+
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) =>{
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
 
 module.exports = app;
