1651c161cf3d85ad1e756bdf40887e01925931f9
aminalizadeh.developer <aminalizadeh.developer@gmail.com>
2021-01-16 16:36:15 +0330

    User login added and send a token with jsonWebToken to client

--- api/routes/user.js
+++ api/routes/user.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
 const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
 
@@ -44,6 +45,52 @@ router.post('/signup', (req, res, next) => {
                     }
                 });
             }
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({
+                error: error
+            });
+        });
+});
+
+router.post('/login', (req, res, next) => {
+    User.find( {email: req.body.email} )
+        .exec()
+        .then(user => {
+            if (user.length < 1) {
+                return res.status(401).json({
+                    message: 'Auth faild'
+                });
+            }
+            bcrypt.compare(req.body.password, user[0].password, (error, result) => {
+                if (error) {
+                    console.log(error);
+                    res.status(500).json({
+                        error: error
+                    });
+                }
+                const token = jwt.sign(
+                    {
+                        email: user[0].email,
+                        userId: user[0]._id
+                    },
+                    process.env.JWT_KEY,
+                    {
+                        expiresIn: '1h'
+                    }
+                );
+                if (result) {
+                    return res.status(201).json({
+                        message: 'Auth successfully',
+                        token: token
+                    });
+                }
+
+                res.status(401).json({
+                    message: 'Auth faild'
+                });
+            });
         })
         .catch(error => {
             console.log(error);
--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.use((req, res, next) => {
     }
 
     next();
-})
+});
 
 app.use('/products', productsRoutes);
 app.use('/orders', ordersRoutes);
